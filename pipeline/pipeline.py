5d69cdd7bc26170d5a71a16671f0aec5c673560b
Marcus <marcus.moura@ipnet.cloud>
2024-03-18 10:54:02 -0300

    feat: create trusted scan

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,33 +1,65 @@
-import duckdb
 import os
-from bigquery_load_data import BigQueryLoadData
+from bigquery import BigQueryLoadData, bigquery_run_query_to_dataframe, get_bigquery_client
 from soda.scan_operations import run_soda_scan
-
-# BigQuery variables
-project_id = os.getenv("PROJECT_ID")
-dataset_id = os.getenv("DATASET_ID")
-location = os.getenv("LOCATION")
-table_id = "orders"
-write_disposition="WRITE_TRUNCATE"
-
-# Soda variables
-data_source = "bigquery_soda"
-checks_subpath = "ingest"
-scan_name = "orders"
-
-path_file = "data/orders.csv"
-
-df_duck = duckdb.read_csv(path_file).df()
-
-load_bq = BigQueryLoadData(
-            data_source=df_duck,
-            project_id=project_id,
-            dataset_id=dataset_id,
-            table_name=table_id,
-            write_disposition=write_disposition,
-            location = location
-        )
-
-run_soda_scan(data_source=data_source, scan_name=scan_name, checks_subpath=checks_subpath)
+import pandas as pd
+from duckdb_operations import duck_load_csv_to_dataframe, duck_transform_data
 
 
+def main():
+    project_id = os.getenv("PROJECT_ID")
+    dataset_id = os.getenv("DATASET_ID")
+    location = os.getenv("LOCATION")
+    client_bq = get_bigquery_client(project_id)
+    path_file = "data/orders.csv"
+    write_disposition="WRITE_TRUNCATE"
+    data_source_soda = "bigquery_soda"
+
+    # Variables ingest
+    table_ingest = "raw_orders"
+    scan_name_ingest = "orders"
+    checks_subpath_ingest = "ingest"
+    
+    # Variables transform
+    table_transform = "trusted_orders"
+    scan_name_transform = "orders"
+    checks_subpath_transform = "transform"
+    
+    df_duck_raw = duck_load_csv_to_dataframe(path_file)
+    
+    BigQueryLoadData(
+                data_source=df_duck_raw,
+                project_id=project_id,
+                dataset_id=dataset_id,
+                table_name=table_ingest,
+                write_disposition=write_disposition,
+                location = location
+    )
+
+    run_soda_scan(
+        data_source=data_source_soda, 
+        scan_name=scan_name_ingest, 
+        checks_subpath=checks_subpath_ingest
+    )
+
+    df_bigquery_raw = bigquery_run_query_to_dataframe(
+            query=f"SELECT * FROM {project_id}.{dataset_id}.{location}", 
+            bigquery_client=client_bq
+    )
+    
+    df_duck_transform = duck_transform_data(df_bigquery_raw)
+    
+    BigQueryLoadData(
+                data_source=df_duck_transform,
+                project_id=project_id,
+                dataset_id=dataset_id,
+                table_name=table_transform,
+                write_disposition=write_disposition,
+                location = location
+    )
+    
+    run_soda_scan(
+        data_source=data_source_soda, 
+        scan_name=scan_name_transform, 
+        checks_subpath=checks_subpath_transform
+    )
+    
