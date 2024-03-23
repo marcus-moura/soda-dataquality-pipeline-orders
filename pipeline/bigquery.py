d802ce9256b816089640d67b8fa1a55cd02785ab
Marcus <marcus.moura@ipnet.cloud>
2024-03-23 18:16:28 -0300

    chore(bigquery): add comments

--- pipeline/bigquery.py
+++ pipeline/bigquery.py
@@ -44,6 +44,13 @@ class BigQueryDataLoaderError(Exception):
     pass
 
 class BigQueryDataLoader:
+    """
+    Class responsible for loading data into BigQuery.
+    
+    param: project_id (str): The ID of the Google Cloud project.
+    param: location (str): The location of the dataset.
+    param: client (bigquery.Client, optional): The BigQuery client. Defaults to bigquery.Client().
+    """
     def __init__(self, 
         project_id: str, 
         location: str,
@@ -54,7 +61,21 @@ class BigQueryDataLoader:
         self.client = client
 
     def _create_job_config(self, **kwargs) -> bigquery.LoadJobConfig:
-        
+        """
+        Creates a job configuration for loading data into BigQuery.
+
+        param: schema_fields (list, optional): The schema fields for the data. Defaults to None.
+        param: description (str, optional): The table description. Defaults to None.
+        param: create_disposition (str, optional): The table creation disposition. Defaults to 'CREATE_IF_NEEDED'.
+        param: write_disposition (str): The write disposition for the job. Defaults to None.
+        param: autodetect (bool, optional): Whether to automatically detect schema. Defaults to True.
+        param: schema_relax (bool, optional): Whether to allow schema field addition. Defaults to False.
+        param: time_partitioning (str, optional): The type of time partitioning. Defaults to None.
+        param: partition_field (str, optional): The field to partition the data by. Defaults to None.
+
+        Returns:
+            bigquery.LoadJobConfig: The job configuration object.
+        """
         time_partitioning = kwargs.get('time_partitioning')
         if time_partitioning:
             time_partitioning = time_partitioning.upper()
@@ -71,6 +92,12 @@ class BigQueryDataLoader:
         return job_config
 
     def _add_labels(self, destination: str, labels: str) -> None:
+        """
+        Adds labels to a BigQuery table.
+
+        param: destination (str): The destination table.
+        param: labels (str): The labels to be added.
+        """
         table = self.client.get_table(destination)
         existing_labels = table.labels
         if existing_labels != labels:
@@ -79,19 +106,44 @@ class BigQueryDataLoader:
             logger.info(f"Added labels to {table.friendly_name}.")
 
     def _build_destination_table(self, dataset_id: str, table_name: str) -> str:
+        """
+        Builds the destination table name in BigQuery.
+        
+        param: dataset_id (str): The ID of the dataset.
+        param:  table_name (str): The name of the table.
+        
+        Returns:
+            str: The destination table name.
+        """
         return f"{self.project_id}.{dataset_id}.{table_name}"
     
     def _handler_job_result(self, job, destination: str, table_name: str, labels=None):
+        """
+        Handles the result of a job execution.
+
+        param: job: The BigQuery job.
+        param: destination (str): The destination table name.
+        param: table_name (str): The name of the table.
+        param: labels (str, optional): The labels to be added. Defaults to None.
+        """
         try:
-            logger.info(f"Inserting data into the {table_name} in BigQuery!")
+            logger.info(f"Loading data into the table {table_name} in BigQuery!")
             job.result()
-            logger.success("Successfully entered data")
+            logger.success("Data loaded successfully!")
             if labels:
                 self._add_labels(destination, labels)
         except Exception as e:
             raise BigQueryDataLoaderError(f"Failed to insert data into BigQuery: {str(e)}")
         
     def load_json(self, json_rows: dict, dataset_id: str, table_name: str, **kwargs):
+        """
+        Loads JSON data into BigQuery.
+
+        param: json_rows (dict): The JSON data to be loaded.
+        param: dataset_id (str): The ID of the dataset.
+        param: table_name (str): The name of the table.
+        param: **kwargs: Additional keyword arguments for configuring the job.
+        """
         destination = self._build_destination_table(dataset_id, table_name)
         
         job_config = self._create_job_config(**kwargs)
@@ -106,6 +158,14 @@ class BigQueryDataLoader:
         self._handler_job_result(job, destination, table_name, kwargs.get('labels'))
 
     def load_dataframe(self, dataframe: pd.DataFrame, dataset_id: str, table_name: str, **kwargs):
+        """
+        Loads DataFrame data into BigQuery.
+
+        param: dataframe (pd.DataFrame): The DataFrame data to be loaded.
+        param: dataset_id (str): The ID of the dataset.
+        param: table_name (str): The name of the table.
+        param: **kwargs: Additional keyword arguments for configuring the job.
+        """
         destination = self._build_destination_table(dataset_id, table_name)
         
         job_config = self._create_job_config(**kwargs)
@@ -127,6 +187,18 @@ class BigQueryDataLoader:
                 skip_rows: int = 0,
                 **kwargs
         ):
+        """
+        Loads data from a file into BigQuery.
+
+        param: file_source (str): The source file path.
+        param: source_format (str): The format of the source data.
+        param: dataset_id (str): The ID of the dataset.
+        param: table_name (str): The name of the table.
+        param: field_delimiter (str, optional): The field delimiter. Defaults to None.
+        param: encoding (str, optional): The encoding of the file. Defaults to None.
+        param: skip_rows (int, optional): The number of rows to skip. Defaults to 0.
+        param: **kwargs: Additional keyword arguments for configuring the job.
+        """
         destination = self._build_destination_table(dataset_id, table_name)
         
         job_config = self._create_job_config(**kwargs)
