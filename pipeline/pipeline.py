820fb623eb04e086d2864feeec8106e05cadcdaa
Marcus <marcus.moura@ipnet.cloud>
2024-03-23 18:41:17 -0300

    style: organize cod and change comments

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,94 +7,76 @@ import sys
 
 # Configuração do log
 logger.remove()
-logger.add(sys.stdout, 
-           colorize=True, 
-           format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | {level} | {message}", 
+logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | {level} | {message}", 
            level="INFO"
         )
 
-
 def main():
-    # Variáveis de ambiente
+    # Environment variables
     project_id = os.getenv("PROJECT_ID")
     dataset_id = os.getenv("DATASET_ID")
     location = os.getenv("LOCATION")
     
-    # Cliente BigQuery
+    # BigQuery Client
     client_bq = get_bigquery_client(project_id)
     
-    # Caminho do arquivo de origem dos dados
-    path_file_data_source = "input_data/orders.csv"
-    
-    # Configuração de carga
+    # BigQuery Load configurations
     write_disposition = "WRITE_TRUNCATE"
-    
-    # Fonte de dados SODA
-    soda_data_source = "bigquery_soda"
-
-    # Variáveis de ingestão
     table_raw = "raw_orders"
-    scan_name_raw = table_raw
-    checks_subpath_raw = "raw"
-    
-    # Variáveis de transformação
     table_trusted = "trusted_orders"
+
+    # Soda configurations
+    path_file_data_source = "input_data/orders.csv"
+    soda_data_source = "bigquery_soda"
+    scan_name_raw = table_raw
     scan_name_trusted = table_trusted
+    checks_subpath_raw = "raw"
     checks_subpath_trusted = "trusted"
     
-    # Objetos
+    # Objects
     bq_extractor = BigQueryExtractor(client_bq)
     bq_loader = BigQueryDataLoader(project_id, location, client_bq)
     
-    # Carrega os dados brutos do arquivo CSV para o BigQuery
+    # Load raw data from CSV file into BigQuery
     logger.info("Starting order pipeline!")
     logger.info(f"Reading source CSV {path_file_data_source}...")
     df_duck_raw = duck_read_csv_to_dataframe(path_file_data_source)
-    bq_loader.load_dataframe(
-                dataframe=df_duck_raw,
-                dataset_id=dataset_id,
-                table_name=table_raw,
-                write_disposition=write_disposition,
-                location=location
+    
+    bq_loader.load_dataframe(dataframe=df_duck_raw,
+                             dataset_id=dataset_id,
+                             table_name=table_raw,
+                             write_disposition=write_disposition, 
+                             location=location
     )
 
-    # Executa verificação SODA após a ingestão
+    # Run SODA check after ingestion
     logger.info(f"Running SODA Scan {table_raw}...")
-    run_soda_scan(
-        data_source=soda_data_source,
-        scan_name=scan_name_raw,
-        checks_subpath=checks_subpath_raw
-    )
+    run_soda_scan(data_source=soda_data_source, scan_name=scan_name_raw, checks_subpath=checks_subpath_raw)
     logger.success(f"SODA Scan {table_raw} completed!")
 
-    # Extrai os dados brutos do BigQuery para um DataFrame
+    # Extract raw data from BigQuery into a DataFrame
     logger.info("Extracting raw data from BigQuery...")
     df_bigquery_raw = bq_extractor.extract_data_from_bigquery_query(
             query=f"SELECT * FROM {project_id}.{dataset_id}.{table_raw}"
     )
     logger.success("Raw data extracted successfully!")
     
-    # Transforma os dados brutos
+    # Transform the raw data
     logger.info("Transforming the raw data...")
     df_duck_transform = duck_transform_data(df_bigquery_raw)
     logger.success("Raw data successfully transformed!")
     
-    # Carrega os dados transformados para o BigQuery
-    bq_loader.load_dataframe(
-                dataframe=df_duck_transform,
-                dataset_id=dataset_id,
-                table_name=table_trusted,
-                write_disposition=write_disposition,
-                location=location
+    # Load the transformed data into BigQuery
+    bq_loader.load_dataframe(dataframe=df_duck_transform,
+                             dataset_id=dataset_id,
+                             table_name=table_trusted,
+                             write_disposition=write_disposition,
+                             location=location
     )
     
-    # Executa verificação SODA após a transformação
+    # Run SODA check after transformation
     logger.info(f"Running SODA Scan {table_trusted}...")
-    run_soda_scan(
-        data_source=soda_data_source, 
-        scan_name=scan_name_trusted, 
-        checks_subpath=checks_subpath_trusted
-    )
+    run_soda_scan(data_source=soda_data_source, scan_name=scan_name_trusted, checks_subpath=checks_subpath_trusted)
     logger.success(f"SODA Scan {table_trusted} completed!")
 
 if __name__ == "__main__":
